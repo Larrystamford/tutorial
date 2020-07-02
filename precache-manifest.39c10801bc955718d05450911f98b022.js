7b51e001b3c1dfe761360b44677c62be0735fe56
Larry Lee <ftt.larry.lee@grabtaxi.com>
2020-07-02 23:24:02 +0800

    Updates

---
+++ precache-manifest.39c10801bc955718d05450911f98b022.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f18382539289d9bac92401095b0bca85",
+    "url": "/tutorial/index.html"
+  },
+  {
+    "revision": "a973f464aa87a21a37a0",
+    "url": "/tutorial/static/css/main.9d50394a.chunk.css"
+  },
+  {
+    "revision": "b094100ac8df17341bab",
+    "url": "/tutorial/static/js/2.634e2098.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/tutorial/static/js/2.634e2098.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a973f464aa87a21a37a0",
+    "url": "/tutorial/static/js/main.4a5525ee.chunk.js"
+  },
+  {
+    "revision": "3c59371548f94264b147",
+    "url": "/tutorial/static/js/runtime-main.3dca4490.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tutorial/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d63ef61a9ae9ada5e408259edbe77291",
+    "url": "/tutorial/static/media/profilepic.d63ef61a.jpg"
+  }
+]);
